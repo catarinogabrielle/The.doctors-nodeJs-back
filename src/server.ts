6220b5691d780a70edae49ebaae95df6f615b7e1
Thiago Souza <tpimentel368@gmail.com>
2026-02-03 14:02:46 -0600

    Mantendo versão local neste branch

--- src/server.ts
+++ src/server.ts
@@ -19,18 +19,14 @@ app.use((req, res, next) => {
     }
 });
 app.use(cors())
-
 app.use(router);
-
 app.use("/files", express.static(path.resolve(__dirname, '..', 'tmp')))
-
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof Error) {
         return res.status(400).json({
             error: err.message
         })
     }
-
     return res.status(500).json({
         status: 'error',
         message: 'Internal server error.'
