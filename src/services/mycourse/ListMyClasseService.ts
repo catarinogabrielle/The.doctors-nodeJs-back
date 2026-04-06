bb9762b8d784421116d280cbb430b822cbca8430
Thiago Souza <tpimentel368@gmail.com>
2026-04-06 15:49:00 -0300

    fix: add missing link field to ListMyClasseService

--- src/services/mycourse/ListMyClasseService.ts
+++ src/services/mycourse/ListMyClasseService.ts
@@ -13,6 +13,7 @@ class ListMyClasseService {
                 teacherinfo: true,
                 description: true,
                 time: true,
+                link: true,
                 paymentlink: true,
                 category: true
             }
